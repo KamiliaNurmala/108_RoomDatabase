29fa50986a96d85c2a8250a6adee61062c76c2fc
KamiliaNurmala <nurmalakamilia@gmail.com>
2025-11-27 16:49:55 +0700

    Menambahkan fungsi DetailSiswa

--- app/src/main/java/com/example/praktikum9/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/praktikum9/viewmodel/EntryViewModel.kt
@@ -1,9 +1,17 @@
 package com.example.praktikum9.viewmodel
 
+import com.example.praktikum9.room.Siswa
+
 data class DetailSiswa(
     val id: Int = 0,
     val nama: String = "",
     val alamat: String = "",
-    val telepon: String = "",
+    val telpon: String = "",
 )
 
+fun DetailSiswa.toSiswa(): Siswa = Siswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
