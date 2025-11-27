dcc519a25c3005dd8fa2eb961ad84ef4200b2c6d
KamiliaNurmala <nurmalakamilia@gmail.com>
2025-11-27 16:35:16 +0700

    Menambahkan import DatabaseSiswa, Dao dan insert

--- app/src/main/java/com/example/praktikum9/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/praktikum9/room/DatabaseSiswa.kt
@@ -1,6 +1,8 @@
 package com.example.praktikum9.room
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Siswa::class], version = 1, exportSchema = false)
--- app/src/main/java/com/example/praktikum9/room/SiswaDao.kt
+++ app/src/main/java/com/example/praktikum9/room/SiswaDao.kt
@@ -1,5 +1,7 @@
 package com.example.praktikum9.room
 
+import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 
