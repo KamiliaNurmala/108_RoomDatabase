a72aed926a50dbd9b22d84184ce4c92e870f7c5a
KamiliaNurmala <nurmalakamilia@gmail.com>
2025-11-27 16:48:43 +0700

    Menambahkan data class DetailSiswa

---
+++ app/src/main/java/com/example/praktikum9/viewmodel/EntryViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.praktikum9.viewmodel
+
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telepon: String = "",
+)
+
