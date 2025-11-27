a3d7d6f64183ac04241692edcc17df58e68a21ce
KamiliaNurmala <nurmalakamilia@gmail.com>
2025-11-27 16:43:55 +0700

    Menambahkan variable homeUiState beserta StateFlow

--- app/src/main/java/com/example/praktikum9/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/praktikum9/viewmodel/HomeViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.praktikum9.viewmodel
 
 import com.example.praktikum9.repositori.RepositoriSiswa
+import kotlinx.coroutines.flow.StateFlow
 
 class HomeViewModel (private val repositoriSiswa: RepositoriSiswa): viewModel(){
 
@@ -8,4 +9,11 @@ class HomeViewModel (private val repositoriSiswa: RepositoriSiswa): viewModel(){
         private const val TIMEOUT_MILLIS = 5_00L
     }
 
+    val homeUiState: StateFlow<HomeUiState> = repositoriSiswa.getAllSiswaStream()
+            .filterNotNull()
+
+        .map {HomeUiState(listSiswa = it.toList()) }
+        .stateIn(scope - viewModelScope,
+            started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+            initialValue = HomeUiState())
 }
