2e81174123c8ab4d1be91e2d36304f535cc4d771
KamiliaNurmala <nurmalakamilia@gmail.com>
2025-11-27 16:32:35 +0700

    Menambahkan class ContainerApp

--- app/src/main/java/com/example/praktikum9/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/praktikum9/repositori/ContainerApp.kt
@@ -1,5 +1,16 @@
 package com.example.praktikum9.repositori
 
+import android.content.Context
+import com.example.praktikum9.room.DatabaseSiswa
+
 interface ContainerApp {
     val repositoriSiswa: RepositoriSiswa
+}
+
+class ContainerDataApp(private val context: Context) :
+        ContainerApp {
+        override val repositoriSiswa: RepositoriSiswa by lazy {
+             OfflineRepositoriSiswa(
+            DatabaseSiswa.gteDatabase(context).siswaDao())
+        }
 }
