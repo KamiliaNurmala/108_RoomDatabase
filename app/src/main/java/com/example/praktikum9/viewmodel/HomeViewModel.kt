1d6583ed55ba219584e4f1fb237dd3c5e2e0d683
KamiliaNurmala <nurmalakamilia@gmail.com>
2025-11-27 17:05:08 +0700

    Fix import and bug

--- app/src/main/java/com/example/praktikum9/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/praktikum9/viewmodel/HomeViewModel.kt
@@ -1,9 +1,16 @@
 package com.example.praktikum9.viewmodel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.praktikum9.repositori.RepositoriSiswa
+import com.example.praktikum9.room.Siswa
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
-class HomeViewModel (private val repositoriSiswa: RepositoriSiswa): viewModel(){
+class HomeViewModel (private val repositoriSiswa: RepositoriSiswa): ViewModel(){
 
     companion object {
         private const val TIMEOUT_MILLIS = 5_00L
@@ -13,7 +20,11 @@ class HomeViewModel (private val repositoriSiswa: RepositoriSiswa): viewModel(){
             .filterNotNull()
 
         .map {HomeUiState(listSiswa = it.toList()) }
-        .stateIn(scope - viewModelScope,
+        .stateIn(scope = viewModelScope,
             started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
             initialValue = HomeUiState())
+
+    data class HomeUiState (
+        val listSiswa: List<Siswa> = listOf()
+    )
 }
