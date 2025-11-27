f318ce78780e413c7e7843381b110e3874a760ee
KamiliaNurmala <nurmalakamilia@gmail.com>
2025-11-27 19:43:22 +0700

    Fix import and bug

--- app/src/main/java/com/example/praktikum9/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/praktikum9/viewmodel/EntryViewModel.kt
@@ -1,6 +1,38 @@
 package com.example.praktikum9.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.praktikum9.repositori.RepositoriSiswa
 import com.example.praktikum9.room.Siswa
+
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+            return with(uiState){
+                nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+            }
+    }
+
+    fun updateUIState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+    }
+
+    suspend fun saveSiswa(){
+        if(validasiInput()) {
+            repositoriSiswa.insertSiswa(uiStateSiswa.detailSiswa.toSiswa())
+        }
+    }
+}
+
+data class UIStateSiswa(
+    val detailSiswa: DetailSiswa = DetailSiswa(),
+    val isEntryValid: Boolean = false
+)
 
 data class DetailSiswa(
     val id: Int = 0,
@@ -9,9 +41,22 @@ data class DetailSiswa(
     val telpon: String = "",
 )
 
+fun Siswa.toUIStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
+)
+
 fun DetailSiswa.toSiswa(): Siswa = Siswa(
     id = id,
     nama = nama,
     alamat = alamat,
     telpon = telpon
 )
+fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
+
+
