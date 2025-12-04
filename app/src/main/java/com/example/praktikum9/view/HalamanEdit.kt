a2c47ea0dc6f16d1ea05db8e90969efa2e68c313
KamiliaNurmala <nurmalakamilia@gmail.com>
2025-12-04 20:10:53 +0700

    Menambahkan import di HalamanEdit

---
+++ app/src/main/java/com/example/praktikum9/view/HalamanEdit.kt
@@ -0,0 +1,15 @@
+package com.example.praktikum9.view
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.praktikum9.view.route.DestinasiEditSiswa
+import com.example.praktikum9.viewmodel.EditViewModel
+import com.example.praktikum9.viewmodel.provider.PenyediaViewModel
+import kotlinx.coroutines.launch
+
