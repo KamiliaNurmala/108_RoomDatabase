13a08ac4a883bae3be11847d85f98bb2f3238b72
KamiliaNurmala <nurmalakamilia@gmail.com>
2025-12-04 18:29:57 +0700

    Menambahkan function updateSiswa di interface RepositoriSiswa

--- app/src/main/java/com/example/praktikum9/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/praktikum9/repositori/RepositoriSiswa.kt
@@ -11,6 +11,8 @@ interface RepositoriSiswa {
     fun getSiswaStream(id: Int): Flow<Siswa?>
 
     suspend fun deleteSiswa(siswa: Siswa)
+
+    suspend fun updateSiswa(siswa: Siswa)
 }
 
 class OfflineRepositoriSiswa(
