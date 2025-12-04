a22d314872695295783e55ae19b2b66688f8dd74
KamiliaNurmala <nurmalakamilia@gmail.com>
2025-12-04 18:14:17 +0700

    tambah argumen itemIdArg dan routeWithArgs pada DestinasiEditSiswa

--- app/src/main/java/com/example/praktikum9/view/route/DestinasiEditSiswa.kt
+++ app/src/main/java/com/example/praktikum9/view/route/DestinasiEditSiswa.kt
@@ -5,5 +5,6 @@ import com.example.praktikum9.R
 object DestinasiEditSiswa : DestinasiNavigasi {
     override val route = "item_edit"
     override val titleRes = R.string.edit_siswa
-
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
 }
