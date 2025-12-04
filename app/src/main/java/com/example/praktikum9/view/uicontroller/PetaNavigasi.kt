ccd01fdbb8dc6271434ab66874eeb7305ea089b7
KamiliaNurmala <nurmalakamilia@gmail.com>
2025-12-04 16:29:25 +0700

    Menambahkan navigateToItemUpdate dan import di PetaNavigasi

--- app/src/main/java/com/example/praktikum9/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/praktikum9/view/uicontroller/PetaNavigasi.kt
@@ -4,11 +4,16 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.praktikum9.view.DetailSiswaScreen
 import com.example.praktikum9.view.EntrySiswaScreen
 import com.example.praktikum9.view.HomeScreen
+import com.example.praktikum9.view.route.DestinasiDetailSiswa
+import com.example.praktikum9.view.route.DestinasiDetailSiswa.itemIdArg
 import com.example.praktikum9.view.route.DestinasiEntry
 import com.example.praktikum9.view.route.DestinasiHome
 
@@ -28,10 +33,16 @@ fun HostNavigasi(
         composable(DestinasiHome.route) {
             HomeScreen(
                 navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+
+                navigateToItemUpdate = {
+                    navController.navigate("${DestinasiDetailSiswa.route}/${it}")
+                }
             )
         }
         composable(DestinasiEntry.route){
             EntrySiswaScreen(navigateBack = { navController.popBackStack()})
         }
+
+
     }
 }
